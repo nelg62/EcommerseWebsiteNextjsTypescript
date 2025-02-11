85767d66001064c94c9dd1a88ac93392936864c6
nelg62 <glen.harding.nz@gmail.com>
2025-02-11 17:15:12 +1300

    working on stryling the light and dark mode themes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,20 +6,29 @@ import ThemeToggle from "./ThemeToggle";
 const NavBar = () => {
   console.log("NavBar rendered");
   return (
-    <nav className="bg-gray-800 p-4 flex justify-between">
+    <nav className="bg-gray-300 dark:bg-gray-800 p-4 flex justify-between">
       <ul className="flex space-x-4">
         <li>
-          <Link href="/" className="text-white hover:underline">
+          <Link
+            href="/"
+            className="text-gray-600 dark:text-gray-300 hover:underline"
+          >
             Home
           </Link>
         </li>
         <li>
-          <Link href="/products" className="text-white hover:underline">
+          <Link
+            href="/products"
+            className="text-gray-600 dark:text-gray-300 hover:underline"
+          >
             Products
           </Link>
         </li>
         <li>
-          <Link href="/cart" className="text-white hover:underline">
+          <Link
+            href="/cart"
+            className="text-gray-600 dark:text-gray-300 hover:underline"
+          >
             Cart
           </Link>
         </li>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,12 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // background: "var(--background)",
+        // foreground: "var(--foreground)",
+        lightBg: "#ffffff",
+        darkBg: "#121212",
+        lightText: "#1a1a1a",
+        darkText: "#f5f5f5",
       },
     },
   },
